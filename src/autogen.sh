0935b93ae43e25d6f19d759ea097ae3d2216bb8d
Matt Benjamin <matt@linuxbox.com>
2010-11-04 11:47:23 -0400

    Update git submodules in autogen.sh.

--- src/autogen.sh
+++ src/autogen.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# update/fetch linked git modules
+cd ..
+git submodule init
+git submodule update
+cd -
+
+# configure source tree
 rm -f config.cache
 aclocal -I m4
 libtoolize --force --copy
